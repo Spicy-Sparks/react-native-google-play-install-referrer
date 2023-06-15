932bad31e38f093c768643bd611470023a8de685
Slavko Josipović <slavkoj6@gmail.com>
2023-06-15 16:08:15 +0200

    fixes and remove unused stuff

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import { NativeModules } from 'react-native';
+
+export type ReferrerDetails = {
+  url: string;
+  clickTime: string;
+  appInstallTime: string;
+  instantExperienceLaunched: boolean;
+};
+
+
+export function getReferrer(): Promise<ReferrerDetails> {
+  return NativeModules.PlayInstallReferrerModule.getReferrer
+}
+
