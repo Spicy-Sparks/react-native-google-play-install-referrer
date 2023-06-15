ba1d60fcacb566a1d74055ab767bfcdb726ac061
Slavko Josipović <slavkoj6@gmail.com>
2023-06-15 17:33:59 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-
-import { NativeModules } from 'react-native';
+import { NativeModules } from 'react-native'
 
 export type ReferrerDetails = {
-  url: string;
-  clickTime: string;
-  appInstallTime: string;
-  instantExperienceLaunched: boolean;
-};
-
-
+  url: string
+  clickTime: string
+  appInstallTime: string
+  instantExperienceLaunched: boolean
+}
 export function getReferrer(): Promise<ReferrerDetails> {
   return NativeModules.PlayInstallReferrerModule.getReferrer
 }
-
